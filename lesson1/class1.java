5391ba8865606c190e83e8cef274dc0f821a2b33
kindzadzanets <kindzadzanetz@gmail.com>
2024-02-04 20:37:30 +0300

    first lesson finished - print/println (second_try)

--- lesson1/class1.java
+++ lesson1/class1.java
@@ -2,7 +2,24 @@ package lesson1;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.print("Hello mutterFuck!");
+//  метод println - вывод параметра на экран с переходом курсора на новую строку
+        System.out.println("Hello mutterFuck!");
+//  метод print - вывод параметра как он есть
+        System.out.println("Hello");
+
+//  пример: заисать буквы парами в столбик
+        System.out.print("A ");
+        System.out.println(" a");
+        System.out.print("B ");
+        System.out.println(" b");
+        System.out.print("C ");
+        System.out.println(" c");
+
+//  пример: пустые строки между выводом значений
+        System.out.println("abcd");
+        System.out.println();
+        System.out.println("efgh");
+
 
     }
 
---
+++ lesson1/domashka.java
@@ -0,0 +1,12 @@
+public class domashka {
+
+        public static void main(String[] args) {
+                //  Задачка: записать рубаи(четверостишие)
+                System.out.println("                РУБАИ     ");
+                System.out.println("Много лет размышлял я над жизнью земной.");
+                System.out.println("Много лет размышлял я над жизнью земной.");
+                System.out.println("Много лет размышлял я над жизнью земной.");
+                System.out.println("                                    Хуям");
+        }
+
+}
